add98ffae90dbfccf820c5ed4d43243f0b337043
Kuborsky Pavel <p.kuborsky@icloud.com>
2024-06-14 02:19:40 +0800

    Change settings.py

--- nail_studio/nail_studio/settings.py
+++ nail_studio/nail_studio/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DJANGO_DEBUG')
+DEBUG = False if os.getenv('DJANGO_DEBUG') == 'False' else True 
 
 ALLOWED_HOSTS = [*os.getenv('ALLOWED_HOSTS').split(' ')]
 
